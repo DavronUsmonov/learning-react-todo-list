d49cfd92a4a59eb44310f0ccb80d2ae2a3b498d8
Davron Usmonov <davron.usmonov9@gmail.com>
2022-08-27 13:29:15 -0400

    complete and delete all functions complete

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,7 +2,10 @@ import React from "react"
 import TodoList from "./TodoList";
 import {v4 as uuidv4} from "uuid";
 
+let allCompleted = false;
+
 function TodoForm({ setInputText, todos, setTodos, inputText }) {
+
     function inputTextHandler(e) {
         console.log(e.target.value);
         setInputText(e.target.value);
@@ -14,11 +17,39 @@ function TodoForm({ setInputText, todos, setTodos, inputText }) {
         setInputText("");
     }
 
+    function completeAllHandler(e) {
+        e.preventDefault();
+        for(var i = 0; i < todos.length; i++) {
+            if(todos[i].completed == false) {
+                allCompleted = false;
+                break;
+            }
+            allCompleted = true;
+        }
+        setTodos(todos.map((el) => {
+            el.completed = !allCompleted;
+            return el;
+        }));
+    }
+
+    function deleteAllHandler(e) {
+        e.preventDefault();
+        setTodos([]);
+    }
+
+
+
     return (
-        <form className="todo-form">
-            <input value={inputText} onChange={inputTextHandler} type="text" className ="todo-input"/>
-            <button className="submitButton" onClick={submitTodoHandler} type="submit"/>
-        </form>
+        <div>
+            <form className="todo-form">
+                <input value={inputText} onChange={inputTextHandler} type="text" className ="todo-input"/>
+                <button className="submitButton" onClick={submitTodoHandler} type="submit"/>
+            </form>
+            <form className="all">
+                <button type="submit" onClick={completeAllHandler}>Complete all tasks</button>
+                <button type="submit" onClick={deleteAllHandler}>Delete all tasks</button>
+            </form>
+        </div>
     )
 }
 
